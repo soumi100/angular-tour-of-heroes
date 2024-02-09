50f61f5aaf6e7adbb2a7aa71e05348ef51d4372a
soumia bouhouri <s.bouhouri@virtualvaults.com>
2024-02-09 08:20:18 +0100

    using of

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
